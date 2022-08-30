5281743480f7b347f078212dfce0fda36a997921
Adelle Pitsas <apitsas4@gmail.com>
2022-08-30 16:48:44 -0400

    complete all of the tweet tests

--- tweet.js
+++ tweet.js
@@ -1,5 +1,27 @@
 class Tweet {
-
+ constructor(user, message, link) {
+   this.sender = user
+   this.content = message
+   if (link === undefined) {
+     this.link = 'no link provided'
+   } else {
+     this.link = link
+   }
+   this.likes = 0
+   this.retweets = 0
+   this.replies = []
+   this.replyCount = 0
+ }
+ like() {
+   this.likes++
+ }
+ retweet() {
+   this.retweets++
+ }
+ reply(replyMessage) {
+   this.replies.push(replyMessage)
+   this.replyCount++
+ }
 }
 
 module.exports = Tweet;
